bc2a01248af631afb458373a5c8d1423ab57fb34
Zafeeruddin <mohammed.xafeer@gmail.com>
2024-01-16 14:33:10 +0530

    tried env

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,7 @@
 const mongoose=require("mongoose")
 const { addTodo } = require("./types")
 //connect to database
-
 mongoose.connect("mongodb+srv://mohammedxafeer:No9YrDQ5MqHNx10w@cluster0.ze6jwfk.mongodb.net/Todos")
-//mongoose.connect(process.env.MONGO_URL)
 const todos=mongoose.model('Todos',{
     id:Number,
     title:String,
